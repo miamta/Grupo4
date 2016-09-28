33b501fd7aa5b8aa63cb97736b8f5f62a6063b11
Your Name <you@example.com>
2016-09-28 10:25:24 +0200

    primer commit

---
+++ miguel11.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercicios Git</title>
+  </head>
+  <body>
+    <?php
+ echo "Miguel";
+ echo "amores";
+ echo "Tamarit";
+     ?>
+     <h1> Primera prueba con el git</h1>
+     <h2> Segunda prueba en la clonacion </h2>
+  </body>
+</html>
